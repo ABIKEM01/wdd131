cb039ef57bf17174b1d5ffc40218c7aeb9c5e0ae
Abiodun Babayode <babayod@yahoo.com>
2025-09-12 13:01:31 +0100

    temple images week02

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
+
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector(
+  "#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
---
+++ week02/scripts/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+  if (input.value.trim() !== "") {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.setAttribute("aria-label", `Remove ${input.value}`);
+
+    li.append(deleteButton);
+
+    list.append(li);
+
+    input.value = "";
+
+    input.focus();
+
+    deleteButton.addEventListener("click", () => {
+      list.removeChild(li);
+      input.focus();
+    });
+  } else {
+    alert("Please enter a chapter before adding!");
+    input.focus();
+  }
+});
