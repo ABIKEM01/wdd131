ad03f993ff78a5f7c502b6639669b5e790b5d01c
Abiodun Babayode <babayod@yahoo.com>
2025-09-22 21:34:25 +0100

    W04 Assignment: Picture Album Enhancement

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,12 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// let names = ["Nancy", "Blessing", "Jorge", "Svetlana", "Bob"];
+
+// const filteredName = names.filter((name) => name.charAt(0) === "B");
+// console.log(filteredName);
+
+// mappedArray = names.map((name) => {
+//   const result = name.length;
+//   console.log(result);
+// });
